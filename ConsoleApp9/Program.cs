6f8dd3e8c54a8fb718cbe4122c04c55b97e0ecff
Atkmediastudio <127044847+atkmedia0611@users.noreply.github.com>
2023-05-05 15:05:03 +0700

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("xinchao");
+        }
+    }
+}
